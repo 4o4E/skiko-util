d00f1540b2b8cea5728032ca76f2fa81319a8bf2
4o4E <869951226@qq.com>
2023-10-16 20:34:28 +0800

    修正blue archive图片生成的样式

--- skiko-util-core/src/main/kotlin/generator/list/BlueArchiveGenerator.kt
+++ skiko-util-core/src/main/kotlin/generator/list/BlueArchiveGenerator.kt
@@ -8,14 +8,12 @@ import org.jetbrains.skia.svg.SVGDOM
 import top.e404.skiko.frame.Frame
 import top.e404.skiko.generator.ImageGenerator
 import top.e404.skiko.util.*
+import kotlin.math.max
 
 object BlueArchiveGenerator : ImageGenerator {
     private val families = arrayOf("GlowSansSC-Normal-Heavy.otf", "RoGSanSrfStd-Bd.otf")
     private const val FONT_SIZE = 80F
-    private const val ICON_HEIGHT = 80F
-    private val bg by lazy {
-        SVGDOM(Data.makeFromBytes(getJarFile(this.javaClass, "statistic/blue_archive/bg.svg")))
-    }
+    private const val FONT_SIZE_BOTTOM = 40F
     private val cross by lazy {
         SVGDOM(Data.makeFromBytes(getJarFile(this.javaClass, "statistic/blue_archive/cross.svg")))
     }
@@ -26,11 +24,14 @@ object BlueArchiveGenerator : ImageGenerator {
     override suspend fun generate(args: MutableMap<String, String>): MutableList<Frame> {
         val l = args["l"] ?: ""
         val r = args["r"] ?: ""
-        val x = args["x"]?.toFloatOrNull() ?: 0F
-        val y = args["y"]?.toFloatOrNull() ?: 0F
+        val b = args["b"] ?: ""
+        val r1 = r.substring(0, 1)
+        val r2 = r.substring(1)
+        // val x = args["x"]?.toFloatOrNull() ?: 0F
+        // val y = args["y"]?.toFloatOrNull() ?: 0F
         val lStyle = TextStyle()
             .setFontFamilies(families)
-            .setColor(0xff357cc0.toInt())
+            .setColor(0xff128afa.toInt())
             .setFontStyle(FontStyle(FontWeight.BOLD, FontWidth.NORMAL, FontSlant.OBLIQUE))
             .setFontSize(FONT_SIZE)
             .setBackground(Paint().apply { color = Colors.WHITE.argb })
@@ -40,9 +41,23 @@ object BlueArchiveGenerator : ImageGenerator {
             .setFontStyle(FontStyle(FontWeight.BOLD, FontWidth.NORMAL, FontSlant.OBLIQUE))
             .setFontSize(FONT_SIZE)
             .setBackground(Paint().apply { color = Colors.WHITE.argb })
+        val bStyle = TextStyle()
+            .setFontFamilies(families)
+            .setColor(0xff2b2b2b.toInt())
+            .setFontStyle(FontStyle(FontWeight.BOLD, FontWidth.NORMAL, FontSlant.OBLIQUE))
+            .setFontSize(FONT_SIZE_BOTTOM)
         val w = ParagraphBuilder(ParagraphStyle(), Fonts.fonts)
             .pushStyle(lStyle)
             .addText(l)
+            .build()
+            .layout(10000F)
+            .maxIntrinsicWidth
+        val w2 = ParagraphBuilder(ParagraphStyle(), Fonts.fonts)
+            .pushStyle(lStyle)
+            .addText(l)
+            .popStyle()
+            .pushStyle(rStyle)
+            .addText(r1)
             .build()
             .layout(10000F)
             .maxIntrinsicWidth
@@ -51,20 +66,33 @@ object BlueArchiveGenerator : ImageGenerator {
             .addText(l)
             .popStyle()
             .pushStyle(rStyle)
-            .addText(r)
+            .addText(r1)
+            .addText("\u2005")
+            .addText(r2)
+            .build()
+            .layout(10000F)
+        val paragraphBottom = ParagraphBuilder(ParagraphStyle(), Fonts.fonts)
+            .pushStyle(bStyle)
+            .addText(b)
             .build()
             .layout(10000F)
         val image = Surface.makeRasterN32Premul(
-            (paragraph.maxIntrinsicWidth + 2 * FONT_SIZE).toInt(),
-            (paragraph.height + ICON_HEIGHT + 2 * FONT_SIZE).toInt()
+            (max(paragraph.maxIntrinsicWidth, w + paragraphBottom.maxIntrinsicWidth) + 2 * FONT_SIZE).toInt(),
+            (paragraph.height + 320F).toInt()
         ).fill(Colors.WHITE.argb).withCanvas {
+            val tdx = w2 - 30
+            val tdy = 50F
             save()
-            translate(w - FONT_SIZE * 4.5F, FONT_SIZE * -4F)
+            translate(tdx, tdy)
             ring.render(this)
             restore()
-            paragraph.paint(this, FONT_SIZE, FONT_SIZE + ICON_HEIGHT)
-            translate(w - FONT_SIZE * 4.5F + x, FONT_SIZE * -4F + y)
-            bg.render(this)
+            paragraph.paint(this, FONT_SIZE, FONT_SIZE + 80F)
+            paragraphBottom.paint(
+                this,
+                w2 + 80,
+                paragraph.height + paragraphBottom.height + 100F
+            )
+            translate(tdx, tdy)
             cross.render(this)
         }
         return mutableListOf(Frame(0, image))
