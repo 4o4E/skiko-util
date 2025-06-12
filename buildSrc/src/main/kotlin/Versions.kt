0f11f76e83c9d579df3a0c547c7dc1c79dbdbc7d
4o4E <869951226@qq.com>
2025-06-13 00:34:09 +0800

    feat: 更新依赖版本

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
     const val group = "top.e404"
     const val version = "1.1.1"
-    const val kotlin = "1.8.21"
-    const val skiko = "0.7.77"
+    const val kotlin = "2.1.21"
+    const val skiko = "0.9.2"
 }
 
 fun kotlinx(id: String, version: String = Versions.kotlin) = "org.jetbrains.kotlinx:kotlinx-$id:$version"
